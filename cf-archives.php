66aa5f660639f8c96fc24913a6d68bf1f09c271a
sklein <sklein@546ca710-db30-0410-8887-d9dd8e66aff0>
2009-05-28 19:58:26 +0000

    adding 1.2 tag
    git-svn-id: https://git.crowdfave.org/svn/crowdfavorite/code/wp-plugins/cf-archives/tags/1.2@11462 546ca710-db30-0410-8887-d9dd8e66aff0

--- cf-archives.php
+++ cf-archives.php
@@ -3,7 +3,7 @@
 Plugin Name: CF Archives 
 Plugin URI: http://crowdfavorite.com 
 Description: Advanced features for Archives. 
-Version: 1.2b1
+Version: 1.2
 Author: Crowd Favorite
 Author URI: http://crowdfavorite.com
 */

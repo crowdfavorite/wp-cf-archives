26847953cd48a1303f7e20356fd986191a400c11
Jonathan Johnson <jon@crowdfavorite.com>
2012-06-12 10:21:27 -0600

    updated changelog and plugin version number

--- cf-archives.php
+++ cf-archives.php
@@ -3,7 +3,7 @@
 Plugin Name: CF Archives
 Plugin URI: http://crowdfavorite.com
 Description: Advanced features for Archives.
-Version: 1.4.6
+Version: 1.5
 Author: Crowd Favorite
 Author URI: http://crowdfavorite.com
 */
